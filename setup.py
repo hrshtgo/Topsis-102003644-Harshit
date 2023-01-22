e72733ac0a046827eaad9aec46cbfbfb59c0b93e
hrshtgo <101060099+hrshtgo@users.noreply.github.com>
2023-01-23 01:04:35 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'Topis-Harshit-102003644',        
+  packages = ['Topsis-Harshit-102003644'],  
+  version = '1.0',      
+  license='MIT',        
+  description = 'Library for Multiple Criteria Decision Making using Topsis',   
+  author = 'Harshit Gogia',                  
+  author_email = 'hgogia_be20@thapar.edu',   
+  url = 'https://github.com/hrshtgo/Topsis-102003644-Harshit',   
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['Topsis', 'MCDM'],  
+  install_requires=[            
+          'numpy',
+          'pandas',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',    
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
